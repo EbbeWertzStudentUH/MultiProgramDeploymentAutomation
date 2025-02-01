7ef744e3e8353e9e6b801389191eb76919f77c20
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2025-02-01 14:00:15 +0100

    test apps are setup

---
+++ launcher/Launcher.java
@@ -0,0 +1,17 @@
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("hello world from launcher");
+        // try {
+        //     // Start Backend (Java Components)
+        //     ProcessBuilder backend = new ProcessBuilder("java", "-jar", "backend.jar");
+        //     backend.start();
+
+        //     // Start Frontend (NeutralinoJS Executable)
+        //     ProcessBuilder frontend = new ProcessBuilder("frontend-app");
+        //     frontend.start();
+
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+    }
+}
